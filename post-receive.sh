7a3898b6870abbe9f4571417e53550dc4f5dbd27
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2020-09-18 20:50:35 +0200

    Removed debug messags from post-receive test

--- post-receive.sh
+++ post-receive.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
 while read OLDREV NEWREV REFNAME; do
-    printf "debug OLDREV: %s\n" "${OLDREV}"
-    printf "debug NEWREV: %s\n" "${NEWREV}"
-    printf "debug REFNAME: %s\n" "${REFNAME}"
-
     if [[ ${REFNAME} == "refs/heads/master" ]]; then
         printf "Deploying to live\n"
     elif [[ ${REFNAME} == "refs/heads/develop" ]]; then 
         printf "Deploying to staging\n"
-    else
-        printf "Here should not arrive\n"
     fi;
 done
 
