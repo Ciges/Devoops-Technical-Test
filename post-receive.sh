0311740a696f7eba0353b576cbc1424ec242339d
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2020-09-18 20:48:03 +0200

    Added debug messages

--- post-receive.sh
+++ post-receive.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 while read OLDREV NEWREV REFNAME; do
-    if [[ ${REFNAME} == "ref/heads/master" ]]; then
+    printf "debug OLDREV: %s\n" "${OLDREV}"
+    printf "debug NEWREV: %s\n" "${NEWREV}"
+    printf "debug REFNAME: %s\n" "${REFNAME}"
+
+    if [[ ${REFNAME} == "refs/heads/master" ]]; then
         printf "Deploying to live\n"
-    elif [[ ${REFNAME} == "ref/heads/develop" ]]; then 
+    elif [[ ${REFNAME} == "refs/heads/develop" ]]; then 
         printf "Deploying to staging\n"
     fi;
 done
