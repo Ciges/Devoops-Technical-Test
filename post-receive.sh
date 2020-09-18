4f6e26e7248d611b72b78fb1d08204f4b2f6fef8
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2020-09-18 20:49:33 +0200

    Added debug messages

--- post-receive.sh
+++ post-receive.sh
@@ -9,6 +9,8 @@ while read OLDREV NEWREV REFNAME; do
         printf "Deploying to live\n"
     elif [[ ${REFNAME} == "refs/heads/develop" ]]; then 
         printf "Deploying to staging\n"
+    else
+        printf "Here should not arrive\n"
     fi;
 done
 
